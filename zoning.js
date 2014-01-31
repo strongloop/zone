ef31b86271b81b56923374236574f530c4f94d70
Sam Roberts <sam@strongloop.com>
2014-01-31 13:30:52 -0800

    Make zoning requireable as a module

--- zoning.js
+++ zoning.js
@@ -1,3 +1,5 @@
+exports.Zone = Zone;
+exports.Gate = Gate;
 
 var assert = require('assert');
 
@@ -553,51 +555,55 @@ new Zone(function RootZone() {
   });
 
   require('fs').zstat = stat;
-  
-  new Zone(function outer_zone() {
-    console.log('Beginning zone %s', zone.name);
-    
-    var n = 5;
-    var iv = setInterval(function() {
-      console.log('setInterval callback in zone %s. Left: %s', zone.name, n--);
-      if (n === 0)
-        clearInterval(iv);
-    }, 500);
-    
-    new Zone(function inner_zone() {
+
+  if (module.main === module.filename) {
+    new Zone(function outer_zone() {
       console.log('Beginning zone %s', zone.name);
-      setTimeout(function() {
-        console.log('setTimeout callback in zone %s', zone.name);
-      }, 1000);
-    }).setCallback(function(error) {
-      console.log('Zone inner_zone ended, back in %s. Error: ', zone.name, error);
-    });
-    
-    new Zone(function stat_zone() {
-      stat('bla', function(error, stats) {
-        console.log('stat() callback in zone %s. (error, result) = (%s, %s)', zone.name, error, stats);
-        if (error) 
-          throw error;
+
+      var n = 5;
+      var iv = setInterval(function() {
+        console.log('setInterval callback in zone %s. Left: %s', zone.name, n--);
+        if (n === 0)
+          clearInterval(iv);
+      }, 500);
+
+      new Zone(function inner_zone() {
+        console.log('Beginning zone %s', zone.name);
+        setTimeout(function() {
+          console.log('setTimeout callback in zone %s', zone.name);
+        }, 1000);
+      }).setCallback(function(error) {
+        console.log('Zone inner_zone ended, back in %s. Error: ', zone.name, error);
       });
-    }).setCallback(function(error) {
-      console.log('Zone stat_zone ended, back in %s. Error: %s', zone.name, error);
+
+      new Zone(function stat_zone() {
+        stat('bla', function(error, stats) {
+          console.log('stat() callback in zone %s. (error, result) = (%s, %s)', zone.name, error, stats);
+          if (error) 
+            throw error;
+        });
+      }).setCallback(function(error) {
+        console.log('Zone stat_zone ended, back in %s. Error: %s', zone.name, error);
+        if (error) throw error;
+      });
+
+    }).setCallback(function(error, result) {
+      console.log('Zone outer_zone ended, back in %s. Error: %s', zone.name, error);
       if (error) throw error;
     });
-    
-  }).setCallback(function(error, result) {
-    console.log('Zone outer_zone ended, back in %s. Error: %s', zone.name, error);
-    if (error) throw error;
-  });
+  }
 });
 
-new Zone(function() {
-  var ee = new EventEmitter();
-  
+if (module.main === module.filename) {
   new Zone(function() {
-    ee.on('test', function() {
-      console.log('bla');
+    var ee = new EventEmitter();
+
+    new Zone(function() {
+      ee.on('test', function() {
+        console.log('bla');
+      });
     });
+
+    ee.emit('test');
   });
-  
-  ee.emit('test');
-});
+}
