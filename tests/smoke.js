de2cd6897d3f445898a3b21acfa41d820728567a
Sam Roberts <sam@strongloop.com>
2014-02-14 20:08:01 -0800

    Use debuglog for log messages

--- tests/smoke.js
+++ tests/smoke.js
@@ -8,39 +8,39 @@ var Zone = zone.Zone;
 
 tap.test('smoke test', function(t) {
   new Zone(function outer_zone() {
-    console.log('Beginning zone %s', zone.name);
+    tap.log('Beginning zone %s', zone.name);
 
     var n = 5;
     var iv = setInterval(function() {
-      console.log('setInterval callback in zone %s. Left: %s', zone.name, n--);
+      tap.log('setInterval callback in zone %s. Left: %s', zone.name, n--);
       if (n === 0)
         clearInterval(iv);
     }, 50);
 
     new Zone(function inner_zone() {
-      console.log('Beginning zone %s', zone.name);
+      tap.log('Beginning zone %s', zone.name);
       setTimeout(function() {
-        console.log('setTimeout callback in zone %s', zone.name);
+        tap.log('setTimeout callback in zone %s', zone.name);
       }, 100);
     }).setCallback(function(error) {
-      console.log('Zone inner_zone ended, back in %s. Error: ', zone.name, error);
+      tap.log('Zone inner_zone ended, back in %s. Error: ', zone.name, error);
     });
 
     new Zone(function stat_zone() {
       var statZone = this;
       fs.stat('bla', function(error, stats) {
         assert.strictEqual(zone, statZone);
-        console.log('stat() callback in zone %s. (error, result) = (%s, %s)', zone.name, error, stats);
+        tap.log('stat() callback in zone %s. (error, result) = (%s, %s)', zone.name, error, stats);
         if (error)
           throw error;
       });
     }).setCallback(function(error) {
-      console.log('Zone stat_zone ended, back in %s. Error: %s', zone.name, error);
+      tap.log('Zone stat_zone ended, back in %s. Error: %s', zone.name, error);
       if (error) throw error;
     });
 
   }).setCallback(function(error, result) {
-    console.log('Zone outer_zone ended, back in %s. Error: %s', zone.name, error);
+    tap.log('Zone outer_zone ended, back in %s. Error: %s', zone.name, error);
     if (error) throw error;
   });
 
@@ -51,7 +51,7 @@ tap.test('smoke test', function(t) {
 
     new Zone(function() {
       ee.on('test', function() {
-        console.log('bla');
+        tap.log('bla');
       });
     });
 
