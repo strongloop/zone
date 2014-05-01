240b10b68f6b67fb71713abe5ec9675dfd2e413b
Bert Belder <bertbelder@gmail.com>
2014-05-01 20:36:11 +0200

    showcase: add inspect showcase

---
+++ showcase/inspect/demo.js
@@ -0,0 +1,24 @@
+var Zone = require('../../').Zone; // require('zone');
+
+var net = require('net');
+
+new Zone(function ServerZone() {
+  var server = net.createServer(function(conn) {
+    conn.resume();
+  });
+
+  server.listen(3000);
+});
+
+
+for (var i = 0; i < 10; i++) {
+  new Zone(function ConnectionZone() {
+    var conn = net.connect(3000, function() {
+      new Zone(function IntervalZone() {
+        setInterval(function() {
+          conn.write('hello');
+        }, 1);
+      });
+    });
+  });
+}
