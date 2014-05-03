f7ef2ab1c325862a5fddbefdcad4aa949dc24b99
Bert Belder <bertbelder@gmail.com>
2014-05-03 04:30:43 +0200

    test: add test for child_process zone support

---
+++ test/simple/child-process.js
@@ -0,0 +1,68 @@
+
+require('../common.js');
+
+var assert = require('assert');
+var exec = require('child_process').exec;
+var execFile = require('child_process').execFile;
+var spawn = require('child_process').spawn;
+var Zone = zone.Zone;
+
+
+var callbackCount = 0;
+
+process.on('exit', function() {
+  assert(callbackCount === 6);
+});
+
+
+var spawnZone = new Zone(function SpawnZone() {
+  setTimeout(function() {
+    callbackCount++;
+    assert(zone === spawnZone);
+
+    throw new Error('expected error');
+  });
+
+  var p = spawn('cat', ['-']);
+  p.on('close', onClose);
+
+  function onClose(code, signal) {
+    callbackCount++;
+    assert(zone === spawnZone);
+    assert(signal === 'SIGKILL');
+  }
+
+}).catch (function(err) {
+  callbackCount++;
+  assert(zone === zone.root);
+  assert(/expected/.test(err));
+});
+
+
+var execZone = new Zone(function ExecZone() {
+  exec('echo hello world', callback);
+
+  function callback(err, stdout, stderr) {
+    callbackCount++;
+    assert(zone === execZone);
+    assert(!err);
+    assert(/hello world/.test(stdout));
+  }
+});
+
+
+var execFileZone = new Zone(function ExecFileZone() {
+  execFile('inv$alid~file', [], callback);
+
+  function callback(err, stdout, stderr) {
+    callbackCount++;
+    assert(zone === execFileZone);
+    throw err;
+  }
+
+}).catch (function(err) {
+  callbackCount++;
+  assert(err.code === 'ENOENT');
+});
+
+
